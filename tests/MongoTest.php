ea31c8c1afcb0af7cf9fa1dde5eced68ac9537d0
fogelito <fogelshmuel@gmail.com>
2022-11-07 18:30:44 +0200

    revert $useCoroutine

--- tests/MongoTest.php
+++ tests/MongoTest.php
@@ -21,8 +21,7 @@ class MongoTest extends TestCase
             return self::$db;
         }
 
-        // todo: revert true to false
-        $client = new Client('testing', 'mongo', 27017, 'root', 'example', true);
+        $client = new Client('testing', 'mongo', 27017, 'root', 'example', false);
         $client->connect();
 
         self::$db = $client;
